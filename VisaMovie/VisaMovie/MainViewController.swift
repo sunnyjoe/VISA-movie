43b1f3a9e14e0a271f8fd3f4a362f1de609d02de
Jiao <jiaoqing@mozat.com>
2016-07-23 09:23:53 +0800

    use pod install swift son

---
+++ VisaMovie/VisaMovie/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  VisaMovie
+//
+//  Created by Jiao on 23/7/16.
+//  Copyright © 2016 Jiao. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
